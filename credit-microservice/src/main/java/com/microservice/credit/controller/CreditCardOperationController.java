d0ad7b5be381bb56ca886a11032cfb6ed3b2147b
Deyvis <deyvisml2002@gmail.com>
2024-05-23 23:08:57 -0500

    feat: CreditCardOperationController improve.

--- credit-microservice/src/main/java/com/microservice/credit/controller/CreditCardOperationController.java
+++ credit-microservice/src/main/java/com/microservice/credit/controller/CreditCardOperationController.java
@@ -1,11 +1,18 @@
 package com.microservice.credit.controller;
 
+import com.microservice.credit.entity.CreditCardOperation;
+import com.microservice.credit.exception.RequestException;
+import com.microservice.credit.response.BaseResponse;
 import com.microservice.credit.service.CreditCardOperationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/credit-card-operations")
@@ -15,14 +22,32 @@ public class CreditCardOperationController {
     private CreditCardOperationServiceImpl creditCardOperationService;
 
     @GetMapping("")
-    public Object index()
+    public ResponseEntity<BaseResponse<?>> index()
     {
-        return creditCardOperationService.findAll();
+        try {
+            List<CreditCardOperation> creditCardOperations = creditCardOperationService.findAll();
+            BaseResponse<List<CreditCardOperation>> baseResponse = new BaseResponse<>(true, "Credit card operations obtained successfully.", creditCardOperations);
+            return new ResponseEntity<>(baseResponse, HttpStatus.OK);
+        }
+        catch (RequestException ex)
+        {
+            BaseResponse<Void> baseResponse = new BaseResponse<>(false, ex.getMessage(), null);
+            return new ResponseEntity<>(baseResponse, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("/{id}")
-    public Object show(@PathVariable Long id)
+    public ResponseEntity<BaseResponse<?>> show(@PathVariable Long id)
     {
-        return creditCardOperationService.findCreditCardOperationById(id);
+        try {
+            CreditCardOperation creditCardOperation = creditCardOperationService.findCreditCardOperationById(id);
+            BaseResponse<CreditCardOperation> baseResponse = new BaseResponse<>(true, "Credit card operation obtained successfully.", creditCardOperation);
+            return new ResponseEntity<>(baseResponse, HttpStatus.OK);
+        }
+        catch (RequestException ex)
+        {
+            BaseResponse<Void> baseResponse = new BaseResponse<>(false, ex.getMessage(), null);
+            return new ResponseEntity<>(baseResponse, HttpStatus.BAD_REQUEST);
+        }
     }
 }
--- credit-microservice/src/main/java/com/microservice/credit/service/CreditCardOperationServiceImpl.java
+++ credit-microservice/src/main/java/com/microservice/credit/service/CreditCardOperationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.microservice.credit.service;
 
 import com.microservice.credit.entity.CreditCardOperation;
+import com.microservice.credit.exception.RequestException;
 import com.microservice.credit.repository.CreditCardOperationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,11 @@ public class CreditCardOperationServiceImpl implements ICreditCardOperationServi
     public CreditCardOperation findCreditCardOperationById(Long id) {
         Optional<CreditCardOperation> creditCardOperation = creditCardOperationRepository.findOneById(id);
 
-        return creditCardOperation.orElse(null);
+        if (creditCardOperation.isEmpty())
+        {
+            throw new RequestException("There is not a credit card operation fot the id: "+id);
+        }
+
+        return creditCardOperation.get();
     }
 }
